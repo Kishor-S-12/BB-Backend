ae056fa1993cd7b6b5bb052389fdef2d956921ea
Kishor-IT <kishor.s2021it@sece.ac.in>
2025-08-26 11:22:24 +0530

    first commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 const corsOptions = {
-  origin: "http://localhost:5173", // Frontend URL
+  origin: "https://bb-frontend-b0fd.onrender.com", // Frontend URL
   methods: "GET,POST,PUT,DELETE",
   credentials: true, // If you need cookies or auth headers
 };
